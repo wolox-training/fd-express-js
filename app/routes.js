f040cd52a2cf78b92d7a2d336fc2ea9fda7b8626
“Federico <federico.dinucci@wolox.com.ar>
2018-03-21 18:45:38 -0600

    List all Users

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,9 @@
 const userController = require('./controllers/users_controller'),
+  authentication = require('../app/middlewares/authentication'),
   sessionController = require('./controllers/sessions_controller');
 
 exports.init = app => {
   app.post('/users', userController.signUp);
   app.post('/users/sessions', sessionController.logIn);
+  app.get('/users', [authentication.handle], userController.index);
 };
